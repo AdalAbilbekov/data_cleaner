36d30eb9922eb6fa321a59a7ee902e7c72d0d51f
batyr <arysbatyr@gmail.com>
2024-05-04 12:22:27 +0500

    fixed problem with broken segments

--- cleaningFunction.py
+++ cleaningFunction.py
@@ -30,7 +30,7 @@ def getting_number_of_words(data_frame):
 def cleaning_dump_extract_csv(path_to_nutch, path_to_csv, path_to_java_home, path_to_dumps):
     print('The starting process...')
 
-    list_of_segments = sorted(Path(f'{path_to_nutch}/two_k_links/segments').iterdir(), key=os.path.getmtime)
+    list_of_segments = sorted(Path(f'{path_to_nutch}/nutch/segments').iterdir(), key=os.path.getmtime)
     list_of_segments = [str(dir).split('/')[-1] for dir in list_of_segments] 
 
     if len(list_of_segments) > 1:
@@ -64,76 +64,81 @@ def cleaning_dump_extract_csv(path_to_nutch, path_to_csv, path_to_java_home, pat
     
     if len(segments_to_process) > 0 and list_of_segments != 'no segments':
         start = datetime.now()
-        # logger = logging.getLogger(__name__)
+        logger = logging.getLogger(__name__)
         languages = [Language.ENGLISH, Language.ARABIC, Language.ARMENIAN, Language.AZERBAIJANI, Language.CHINESE, Language.KAZAKH, Language.MONGOLIAN, Language.RUSSIAN, Language.TURKISH]
         detector = LanguageDetectorBuilder.from_languages(*languages).build()
         for segment in tqdm.tqdm(segments_to_process):
-            print('Starting the dump extraction process ...')
-            if segment not in csv_files:
-                print(f'Creating dump: {segment}')
-                subprocess.run(f'{path_to_nutch}/bin/nutch readseg -dump {path_to_nutch}/two_k_links/segments/{segment} {path_to_nutch}/{path_to_dumps}/{segment}', shell=True)
-            
-            dump = segment
-
-            with open(f'{path_to_nutch}/{path_to_dumps}/{dump}/dump', 'r') as dump_file:
-                all_content = dump_file.read()
-
-            # splitting the data by Content::
-            print('Starting the cleaning process ...')
-            recno = all_content.split('Recno:')
-
-            uzbek_l1, uzbek_l2, uzbek_l1U, uzbek_l2U = 'ў', 'ҳ', 'ў'.upper(), 'ҳ'.upper()
-            the_data = []
-            for mini_recno in tqdm.tqdm(recno):
-                if 'Version: -1' in mini_recno:
-                    url = mini_recno.split('Version: -1')[1].split('Content:')[0].split('\n')[1]
-                    raw_html = f"{mini_recno.split('Version: -1')[1].split('Content:')[1].split('</html>')[0]}</html>"
-                    # if 'lang="ru"' in raw_html or 'lang="kk"' in raw_html:
-                    text = trafilatura.extract(raw_html)
-                    if text != None:
-                        if uzbek_l1 in text or uzbek_l2 in text or uzbek_l1U in text or uzbek_l2U in text:
-                            continue
-                        else:
-                            lst_text = text.split('\n')
-                            joint_text = []
-                            # print(text)
-                            for sentence in lst_text:
-                                if len(sentence) > 0 and len(sentence.split()) >= 5:
-                                    joint_text.append(sentence)
-                            joint_text = list(pd.Series(joint_text).drop_duplicates())
-                            cleaned_text = '\n'.join(joint_text)
-                            if len(cleaned_text) > 1 and len(cleaned_text.replace('\n', ' ').split()) > 30:
-                                the_data.append({url:cleaned_text})
-
-            # the_total_cleaned_data.append(all_new_lvl)
-            all_data = []
-
-            for dictionary in the_data:
-                mua = [{"url":key.replace('url: ', ''), "data": value} for key, value in dictionary.items()]
-                all_data += mua           
-
-            df = pd.DataFrame(all_data)
-
-            if df.shape[0] > 0:
-                language_detection = [str(detector.detect_language_of(paragraph)).replace('Language.', '') for paragraph in df['data']]
-                df['language_id'] = language_detection
-                needed_languages = ['ENGLISH', 'KAZAKH', 'RUSSIAN']
-                df_three_languages = df[df['language_id'].isin(needed_languages)]
-                df_three_languages = df_three_languages.drop_duplicates(subset=['data'])
+            try:
+                print('Starting the dump extraction process ...')
+                if segment not in csv_files:
+                    print(f'Creating dump: {segment}')
+                    subprocess.run(f'{path_to_nutch}/bin/nutch readseg -dump {path_to_nutch}/nutch/segments/{segment} {path_to_nutch}/{path_to_dumps}/{segment}', shell=True)
                 
-                all_tokens = getting_number_of_words(df)
-                cleaned_tokens = getting_number_of_words(df_three_languages)
+                dump = segment
 
-                logger = f'Segment: {dump} | Date: {start.strftime("%Y-%m-%d %H:%M:%S")} | Tokens all: {all_tokens} | Tokens cleaned: {cleaned_tokens}'
-                with open('tokkens_info_nurinform.txt', 'a') as txt_file:
-                    txt_file.write("%s\n" % logger)
+                with open(f'{path_to_nutch}/{path_to_dumps}/{dump}/dump', 'r') as dump_file:
+                    all_content = dump_file.read()
 
-                df_three_languages.to_csv(f'{path_to_csv}/{dump}.csv', index=False, encoding= 'utf-8')
+                # splitting the data by Content::
+                print('Starting the cleaning process ...')
+                recno = all_content.split('Recno:')
 
-            if dump in os.listdir(f'{path_to_nutch}/{path_to_dumps}'):
-                print(f'The dump {dump} is deleted ...')
-                subprocess.run(f'rm -rf {path_to_nutch}/{path_to_dumps}/{dump}', shell=True)
+                uzbek_l1, uzbek_l2, uzbek_l1U, uzbek_l2U = 'ў', 'ҳ', 'ў'.upper(), 'ҳ'.upper()
+                the_data = []
+                for mini_recno in tqdm.tqdm(recno):
+                    if 'Version: -1' in mini_recno:
+                        url = mini_recno.split('Version: -1')[1].split('Content:')[0].split('\n')[1]
+                        raw_html = f"{mini_recno.split('Version: -1')[1].split('Content:')[1].split('</html>')[0]}</html>"
+                        # if 'lang="ru"' in raw_html or 'lang="kk"' in raw_html:
+                        text = trafilatura.extract(raw_html)
+                        if text != None:
+                            if uzbek_l1 in text or uzbek_l2 in text or uzbek_l1U in text or uzbek_l2U in text:
+                                continue
+                            else:
+                                lst_text = text.split('\n')
+                                joint_text = []
+                                # print(text)
+                                for sentence in lst_text:
+                                    if len(sentence) > 0 and len(sentence.split()) >= 5:
+                                        joint_text.append(sentence)
+                                joint_text = list(pd.Series(joint_text).drop_duplicates())
+                                cleaned_text = '\n'.join(joint_text)
+                                if len(cleaned_text) > 1 and len(cleaned_text.replace('\n', ' ').split()) > 30:
+                                    the_data.append({url:cleaned_text})
 
+                # the_total_cleaned_data.append(all_new_lvl)
+                all_data = []
+
+                for dictionary in the_data:
+                    mua = [{"url":key.replace('url: ', ''), "data": value} for key, value in dictionary.items()]
+                    all_data += mua           
+
+                df = pd.DataFrame(all_data)
+
+                if df.shape[0] > 0:
+                    language_detection = [str(detector.detect_language_of(paragraph)).replace('Language.', '') for paragraph in df['data']]
+                    df['language_id'] = language_detection
+                    needed_languages = ['ENGLISH', 'KAZAKH', 'RUSSIAN']
+                    df_three_languages = df[df['language_id'].isin(needed_languages)]
+                    df_three_languages = df_three_languages.drop_duplicates(subset=['data'])
+                    
+                    all_tokens = getting_number_of_words(df)
+                    cleaned_tokens = getting_number_of_words(df_three_languages)
+
+                    logger_txt = f'Segment: {dump} | Date: {start.strftime("%Y-%m-%d %H:%M:%S")} | Tokens all: {all_tokens} | Tokens cleaned: {cleaned_tokens}'
+                    with open('tokkens_info_nurinform.txt', 'a') as txt_file:
+                        txt_file.write("%s\n" % logger_txt)
+
+                    df_three_languages.to_csv(f'{path_to_csv}/{dump}.csv', index=False, encoding= 'utf-8')
+
+                if dump in os.listdir(f'{path_to_nutch}/{path_to_dumps}'):
+                    print(f'The dump {dump} is deleted ...')
+                    subprocess.run(f'rm -rf {path_to_nutch}/{path_to_dumps}/{dump}', shell=True)
+            except:
+                logger.error(f'The dump is broken {segment}')
+                error_message = f'The dump is broken {segment}'
+                with open('the_segment_with_error.txt', 'a') as txt_file:
+                    txt_file.write("%s\n" % error_message)
         end = datetime.now()
         td = (end - start).total_seconds() / 60
         print(f"The time of execution of above program is : {td:.03f}min")
@@ -149,7 +154,7 @@ if __name__ == "__main__":
     csv_d = args.csv
     iteration = 0
     size = os.get_terminal_size() 
-    # logging.basicConfig(filename='cleaning.log', encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig(filename='cleaning.log', encoding='utf-8', level=logging.DEBUG)
     while True:
         iteration += 1
         print(f'New iteration has started, The iteration: {iteration}')
--- utils.py
+++ utils.py
@@ -2,13 +2,13 @@ import argparse
 
 def get_arguments_for_cleaning():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--nutch_path', type=str, default='/mnt/storage/crowl/adal_work/crawling/nutch',
+    parser.add_argument('-n', '--nutch_path', type=str, default='/home/batyr/workspace/crawling/nutch',
                         help='The path  to your Apache Nutch')
     parser.add_argument('-j', '--java_env', type=str, default='/usr/lib/jvm/java-11-openjdk-amd64',
                         help='The path  to JAVA HOME')
     parser.add_argument('-d', '--dumps', type=str, default='nutch_core_segment_dumps',
                         help='The directory to save dumps')
-    parser.add_argument('-c', '--csv', type=str, default='/mnt/storage/crowl/adal_work/two_k_links',
+    parser.add_argument('-c', '--csv', type=str, default='/home/batyr/workspace/all_links',
                         help='The directory to save CSV files')
     
     args = parser.parse_args()
